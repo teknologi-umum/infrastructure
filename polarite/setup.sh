b0a82732021a0dcc29506e44ae645836c58686e3
Reinaldy Rafli <aldy505@proton.me>
2024-05-18 12:57:41 +0700

    feat: move to python script, automated deployment (#8)
    * feat: initial move to python script
    
    * feat: missing some files
    
    * feat: move verdaccio to Traefik configuration
    
    * feat: proper running python script
    
    * feat: introduce github actions scripts
    
    * fix: typo on actions script version
    
    * ci: setup script on actions
    
    * fix: editorconfig issue
    
    * fix: another editorconfig issue

---
+++ polarite/setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker volume create polarite-data
--- traefik/setup.sh
+++ traefik/setup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker network create public-web -d bridge --subnet 172.31.0.0/22
+
+docker network craete monitoring
